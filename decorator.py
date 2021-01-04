0e045debec4a43badc027502f264d470a1d806e7
Maxi, Ivanov <iov147259@gmail.com>
2021-01-05 01:13:30 +0500

    another shit)again)0)

--- decorator.py
+++ decorator.py
@@ -1,9 +1,12 @@
 import json
-
+from functools import wraps
 def to_json(func):
-    result = func()
-    return json.dumps(result)
+    @wraps(func)
+    def f(*args, **kwargs):
+        result = func(*args, **kwargs)
+        return json.dumps(result)
 
+    return f
 
 
 
